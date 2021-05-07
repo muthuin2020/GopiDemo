7deece691aadcc935bd268807c1452b2086f0221
Muthuselvam Kandhasamy <mkandhasamy@encoress.com>
2021-05-08 01:30:15 +0530

    Update TestBase.java

--- Pedgog/src/main/java/com/pedgog/web/common/TestBase.java
+++ Pedgog/src/main/java/com/pedgog/web/common/TestBase.java
@@ -4,16 +4,22 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.lang.reflect.Method;
 import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.Reporter;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
 
 import com.pedgog.web.pages.PedgogHomePage;
 import com.pedgog.web.pages.PedgogLoginPage;
@@ -22,41 +28,47 @@ import com.pedgog.web.tests.HomePageTest;
 public class TestBase {
 	public static WebDriver driver;
 	public static boolean isLoggedIn = false;
-	public static String userName, userPassword, homePageTitle;
+	public static String userName, userPassword, homePageTitle, testMethodName;
 	public static Properties prop;
 	PedgogLoginPage loginPage;
 	PedgogHomePage homePage;
-	
+	ITestResult result;
 
 	@BeforeSuite
 	protected void setDrivers() throws InterruptedException {
 		setLoginData();
 		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\drivers\\chromedriver.exe");
 		driver = new ChromeDriver();
-		System.out.println("Launched Google Chrome");			
+		System.out.println("Launched Google Chrome");
 		driver.manage().window().maximize();
 		driver.get("https://coaching.pedgog.in/");
 		System.out.println("Opened Pedgog website");
 		Thread.sleep(2000);
 	}
 
-	public PedgogHomePage loginToPedgog()
-	{
+	@BeforeMethod
+	public void nameBefore(Method method) {
+		System.out.println("========================================================================================");
+		testMethodName = method.getName();
+		System.out.println("Starting Test Method : " + testMethodName);
+	}
+
+	public PedgogHomePage loginToPedgog() {
 		loginPage = new PedgogLoginPage(driver);
 		homePage = new PedgogHomePage(driver);
-		System.out.println("Logging into Pedgog");	
+		System.out.println("Logging into Pedgog");
 		loginPage.enterUserEmail(userName);
-		System.out.println("Entered user email ");			
+		System.out.println("Entered user email ");
 		loginPage.enterPassword(userPassword);
 		System.out.println("Entered user password ");
 		loginPage.clickLogin();
 		System.out.println("Clicked Login button ");
-		if(homePage.getHomePageTtile().equals(homePageTitle)){				
-		isLoggedIn = true;
-		System.out.println("Logged in to Pedgog");		
-		}
-		else
-			System.err.println("------------------------------ Login to Pedgog failed -----------------------------------");
+		if (homePage.getHomePageTtile().equals(homePageTitle)) {
+			isLoggedIn = true;
+			System.out.println("Logged in to Pedgog");
+		} else
+			System.err.println(
+					"------------------------------ Login to Pedgog failed -----------------------------------");
 		return PageFactory.initElements(driver, PedgogHomePage.class);
 	}
 
@@ -81,6 +93,29 @@ public class TestBase {
 		userPassword = prop.getProperty("pwd");
 		homePageTitle = prop.getProperty("homePageTitle");
 
+	}
+
+	@AfterMethod
+	public void afterMethod() {
+
+		result = Reporter.getCurrentTestResult();
+
+		switch (result.getStatus()) {
+		case ITestResult.SUCCESS:
+			System.out.println("Test case " + testMethodName + " is Passed");
+			break;
+
+		case ITestResult.FAILURE:
+			System.err.println("Test case " + testMethodName + " is Failed");
+			break;
+
+		case ITestResult.SKIP:
+			System.out.println("Test case " + testMethodName + " is Skipped");
+			break;
+
+		default:
+			throw new RuntimeException("Invalid status");
+		}
 	}
 
 	@AfterSuite
