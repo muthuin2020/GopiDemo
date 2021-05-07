d38905424af110814c7f5bcbf21239f9371b55d8
Muthuselvam Kandhasamy <mkandhasamy@encoress.com>
2021-05-08 00:31:35 +0530

    Update HelpPage.java

--- Pedgog/src/main/java/com/pedgog/web/pages/HelpPage.java
+++ Pedgog/src/main/java/com/pedgog/web/pages/HelpPage.java
@@ -17,21 +17,6 @@ public class HelpPage extends BasePageAction {
 	@FindBy(xpath = "//button[@title=\"Close\"]")
 	WebElement close;
 
-	@FindBy(xpath = "//a[@href=\"/application/conduct\"]")
-	WebElement conduct;
-
-	@FindBy(xpath = "//a[@href=\"/application/Projects\"]")
-	WebElement project;
-
-	@FindBy(xpath = "//div[@class=\"last-div\"]//*[text()=\"Help\"]")
-	WebElement help;
-
-	@FindBy(xpath = "//div[@class=\"last-div\"]//a[@href=\"/auth/logout\"]")
-	WebElement logout;
-
-	@FindBy(xpath = "//h6[text()=\"Conduct Zone\"]")
-	WebElement conductPageTtile;
-
 	public HelpPage(WebDriver driver) {
 		super(driver);
 		this.driver = driver;
@@ -45,10 +30,9 @@ public class HelpPage extends BasePageAction {
 	public void closeHelpPage() {
 		clickElement(close);
 	}
-	
+
 	public boolean isVideoAvailable() {
 		return isElementVisible(playVideo);
 	}
-	
 
 }
