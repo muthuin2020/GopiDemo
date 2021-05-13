d31fe120ce89aba81f6bdaa22fa5679e9c3d9c2a
Muthuselvam Kandhasamy <mkandhasamy@encoress.com>
2021-05-13 16:06:30 +0530

    Create RunTestNG.java

---
+++ Pedgog/src/main/java/com/pedgog/web/tests/RunTestNG.java
@@ -0,0 +1,18 @@
+package com.pedgog.web.tests;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.testng.TestNG;
+
+public class RunTestNG {
+
+	public static void main(String[] args) {
+		TestNG runner = new TestNG();
+		List<String> suitefiles = new ArrayList<String>();
+		suitefiles.add(System.getProperty("user.dir") + "\\testng.xml");
+		runner.setTestSuites(suitefiles);
+		runner.run();
+	}
+
+}
