2a1a3f467a78f3860c20f1221163b2e198ae852f
Muthuselvam Kandhasamy <mkandhasamy@encoress.com>
2021-09-22 07:09:52 +0530

    adding firefox for students

--- Pedgog/src/main/java/com/pedgog/web/common/MultipleStudents.java
+++ Pedgog/src/main/java/com/pedgog/web/common/MultipleStudents.java
@@ -2,127 +2,250 @@ package com.pedgog.web.common;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class MultipleStudents extends TestBase {
 
 	public void openWindowForStudents(int i) {
 
-		switch (i) {
-		case 2:
-			driverTwo = new ChromeDriver(options);
-			driverTwo.manage().window().maximize();
-			driverTwo.get(myAppURL);
-			break;
+		String browser = "ff";
+		if (browser.equalsIgnoreCase("ch")) {
+			switch (i) {
+			case 2:
+				driverTwo = new ChromeDriver(options);
+				driverTwo.manage().window().maximize();
+				driverTwo.get(myAppURL);
+				break;
 
-		case 3:
-			driverThree = new ChromeDriver(options);
-			driverThree.manage().window().maximize();
-			driverThree.get(myAppURL);
-			break;
+			case 3:
+				driverThree = new ChromeDriver(options);
+				driverThree.manage().window().maximize();
+				driverThree.get(myAppURL);
+				break;
 
-		case 4:
-			driverFour = new ChromeDriver(options);
-			driverFour.manage().window().maximize();
-			driverFour.get(myAppURL);
-			break;
+			case 4:
+				driverFour = new ChromeDriver(options);
+				driverFour.manage().window().maximize();
+				driverFour.get(myAppURL);
+				break;
 
-		case 5:
-			driverFive = new ChromeDriver(options);
-			driverFive.manage().window().maximize();
-			driverFive.get(myAppURL);
-			break;
+			case 5:
+				driverFive = new ChromeDriver(options);
+				driverFive.manage().window().maximize();
+				driverFive.get(myAppURL);
+				break;
 
-		case 6:
-			driverSix = new ChromeDriver(options);
-			driverSix.manage().window().maximize();
-			driverSix.get(myAppURL);
-			break;
+			case 6:
+				driverSix = new ChromeDriver(options);
+				driverSix.manage().window().maximize();
+				driverSix.get(myAppURL);
+				break;
 
-		case 7:
-			driverSeven = new ChromeDriver(options);
-			driverSeven.manage().window().maximize();
-			driverSeven.get(myAppURL);
-			break;
+			case 7:
+				driverSeven = new ChromeDriver(options);
+				driverSeven.manage().window().maximize();
+				driverSeven.get(myAppURL);
+				break;
 
-		case 8:
-			driverEight = new ChromeDriver(options);
-			driverEight.manage().window().maximize();
-			driverEight.get(myAppURL);
-			break;
+			case 8:
+				driverEight = new ChromeDriver(options);
+				driverEight.manage().window().maximize();
+				driverEight.get(myAppURL);
+				break;
 
-		case 9:
-			driverNine = new ChromeDriver(options);
-			driverNine.manage().window().maximize();
-			driverNine.get(myAppURL);
-			break;
+			case 9:
+				driverNine = new ChromeDriver(options);
+				driverNine.manage().window().maximize();
+				driverNine.get(myAppURL);
+				break;
 
-		case 10:
-			driverTen = new ChromeDriver(options);
-			driverTen.manage().window().maximize();
-			driverTen.get(myAppURL);
-			break;
+			case 10:
+				driverTen = new ChromeDriver(options);
+				driverTen.manage().window().maximize();
+				driverTen.get(myAppURL);
+				break;
 
-		case 11:
-			driverEleven = new ChromeDriver(options);
-			driverEleven.manage().window().maximize();
-			driverEleven.get(myAppURL);
-			break;
+			case 11:
+				driverEleven = new ChromeDriver(options);
+				driverEleven.manage().window().maximize();
+				driverEleven.get(myAppURL);
+				break;
 
-		case 12:
-			driverTwelve = new ChromeDriver(options);
-			driverTwelve.manage().window().maximize();
-			driverTwelve.get(myAppURL);
-			break;
+			case 12:
+				driverTwelve = new ChromeDriver(options);
+				driverTwelve.manage().window().maximize();
+				driverTwelve.get(myAppURL);
+				break;
 
-		case 13:
-			driverThirteen = new ChromeDriver(options);
-			driverThirteen.manage().window().maximize();
-			driverThirteen.get(myAppURL);
-			break;
+			case 13:
+				driverThirteen = new ChromeDriver(options);
+				driverThirteen.manage().window().maximize();
+				driverThirteen.get(myAppURL);
+				break;
 
-		case 14:
-			driverFourteen = new ChromeDriver(options);
-			driverFourteen.manage().window().maximize();
-			driverFourteen.get(myAppURL);
-			break;
+			case 14:
+				driverFourteen = new ChromeDriver(options);
+				driverFourteen.manage().window().maximize();
+				driverFourteen.get(myAppURL);
+				break;
 
-		case 15:
-			driverFive = new ChromeDriver(options);
-			driverFive.manage().window().maximize();
-			driverFive.get(myAppURL);
-			break;
+			case 15:
+				driverFive = new ChromeDriver(options);
+				driverFive.manage().window().maximize();
+				driverFive.get(myAppURL);
+				break;
 
-		case 16:
-			driverSix = new ChromeDriver(options);
-			driverSix.manage().window().maximize();
-			driverSix.get(myAppURL);
-			break;
+			case 16:
+				driverSix = new ChromeDriver(options);
+				driverSix.manage().window().maximize();
+				driverSix.get(myAppURL);
+				break;
 
-		case 17:
-			driverSeven = new ChromeDriver(options);
-			driverSeven.manage().window().maximize();
-			driverSeven.get(myAppURL);
-			break;
+			case 17:
+				driverSeven = new ChromeDriver(options);
+				driverSeven.manage().window().maximize();
+				driverSeven.get(myAppURL);
+				break;
 
-		case 18:
-			driverEight = new ChromeDriver(options);
-			driverEight.manage().window().maximize();
-			driverEight.get(myAppURL);
-			break;
+			case 18:
+				driverEight = new ChromeDriver(options);
+				driverEight.manage().window().maximize();
+				driverEight.get(myAppURL);
+				break;
 
-		case 19:
-			driverNineteen = new ChromeDriver(options);
-			driverNineteen.manage().window().maximize();
-			driverNineteen.get(myAppURL);
-			break;
-		case 20:
-			driverTwenty = new ChromeDriver(options);
-			driverTwenty.manage().window().maximize();
-			driverTwenty.get(myAppURL);
-			break;
-		default:
-			break;
+			case 19:
+				driverNineteen = new ChromeDriver(options);
+				driverNineteen.manage().window().maximize();
+				driverNineteen.get(myAppURL);
+				break;
+			case 20:
+				driverTwenty = new ChromeDriver(options);
+				driverTwenty.manage().window().maximize();
+				driverTwenty.get(myAppURL);
+				break;
+			default:
+				break;
+			}
 		}
+		if (browser.equalsIgnoreCase("ff")) {
+			switch (i) {
+			case 2:
+				driverTwo = new FirefoxDriver();
+				driverTwo.manage().window().maximize();
+				driverTwo.get(myAppURL);
+				break;
+
+			case 3:
+				driverThree = new FirefoxDriver();
+				driverThree.manage().window().maximize();
+				driverThree.get(myAppURL);
+				break;
+
+			case 4:
+				driverFour = new FirefoxDriver();
+				driverFour.manage().window().maximize();
+				driverFour.get(myAppURL);
+				break;
+
+			case 5:
+				driverFive = new FirefoxDriver();
+				driverFive.manage().window().maximize();
+				driverFive.get(myAppURL);
+				break;
+
+			case 6:
+				driverSix = new FirefoxDriver();
+				driverSix.manage().window().maximize();
+				driverSix.get(myAppURL);
+				break;
+
+			case 7:
+				driverSeven = new FirefoxDriver();
+				driverSeven.manage().window().maximize();
+				driverSeven.get(myAppURL);
+				break;
+
+			case 8:
+				driverEight = new FirefoxDriver();
+				driverEight.manage().window().maximize();
+				driverEight.get(myAppURL);
+				break;
+
+			case 9:
+				driverNine = new FirefoxDriver();
+				driverNine.manage().window().maximize();
+				driverNine.get(myAppURL);
+				break;
+
+			case 10:
+				driverTen = new FirefoxDriver();
+				driverTen.manage().window().maximize();
+				driverTen.get(myAppURL);
+				break;
+
+			case 11:
+				driverEleven = new FirefoxDriver();
+				driverEleven.manage().window().maximize();
+				driverEleven.get(myAppURL);
+				break;
+
+			case 12:
+				driverTwelve = new FirefoxDriver();
+				driverTwelve.manage().window().maximize();
+				driverTwelve.get(myAppURL);
+				break;
+
+			case 13:
+				driverThirteen = new FirefoxDriver();
+				driverThirteen.manage().window().maximize();
+				driverThirteen.get(myAppURL);
+				break;
+
+			case 14:
+				driverFourteen = new FirefoxDriver();
+				driverFourteen.manage().window().maximize();
+				driverFourteen.get(myAppURL);
+				break;
+
+			case 15:
+				driverFive = new FirefoxDriver();
+				driverFive.manage().window().maximize();
+				driverFive.get(myAppURL);
+				break;
+
+			case 16:
+				driverSix = new FirefoxDriver();
+				driverSix.manage().window().maximize();
+				driverSix.get(myAppURL);
+				break;
+
+			case 17:
+				driverSeven = new FirefoxDriver();
+				driverSeven.manage().window().maximize();
+				driverSeven.get(myAppURL);
+				break;
+
+			case 18:
+				driverEight = new FirefoxDriver();
+				driverEight.manage().window().maximize();
+				driverEight.get(myAppURL);
+				break;
+
+			case 19:
+				driverNineteen = new FirefoxDriver();
+				driverNineteen.manage().window().maximize();
+				driverNineteen.get(myAppURL);
+				break;
+			case 20:
+				driverTwenty = new FirefoxDriver();
+				driverTwenty.manage().window().maximize();
+				driverTwenty.get(myAppURL);
+				break;
+			default:
+				break;
+			}
+		}
+
 	}
 
 	public WebDriver getCurrentStudentsWindow(int i) {
